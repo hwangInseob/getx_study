c6daa969d8a2a25f63593afdf170ee41c374e496
DESKTOP-F0TRJ64\doob9 <hwangis941126@gmail.com>
2024-06-23 09:58:03 +0900

    ROute Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,199 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(fontSize: 32.0),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routingCallback: (value) {
+        debugPrint(value.toString());
+      },
+      home: Home(),
+      getPages: [GetPage(name: '/params/:pathValue', page: () => ParamsPage())],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Get.put(Controller());
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
+        title: Text("홈 페이지 !"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextButton(
+              onPressed: () {
+                Get.to(() => NextPage());
+              },
+              child: Text("Get.to(NextPage())"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextButton(
+              onPressed: () {
+                Get.off(() => NextPage());
+              },
+              child: Text("Get.off(NextPage())"),
+            ),
+            TextButton(
+              onPressed: () {
+                Get.snackbar("this is SnackBar Title", "this is Message");
+              },
+              child: Text("Get.snackbar()"),
+            ),
+            TextButton(
+              onPressed: () {
+                Get.rawSnackbar(
+                    title: "this is SnackBar Title",
+                    message: "this is Message");
+              },
+              child: Text("Get.rawSnackbar()"),
+            ),
+            TextButton(
+              onPressed: () {
+                Get.dialog(MyDialog());
+              },
+              child: Text("Get.dialog(MyDialog())"),
+            ),
+            TextButton(
+              onPressed: () {
+                Get.defaultDialog(title: "title");
+              },
+              child: Text("Get.defaultDialog()"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("This is NextPage()"),
+            Obx(
+              () {
+                if (Get.find<Controller>().routeReturnValue.value.isNotEmpty) {
+                  return Text(
+                      Get.find<Controller>().routeReturnValue.value["key"]);
+                } else {
+                  return Text("go to.ThirdPage and back!");
+                }
+              },
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                Get.find<Controller>().routeReturnValue.value =
+                    await Get.to(ThirdPage(), arguments: "arg1");
+                debugPrint(
+                    Get.find<Controller>().routeReturnValue.value.toString());
+              },
+              child: Text("Get.to(ThirdPage(), arguments: \"arg1\")"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.off(ThirdPage());
+              },
+              child: Text("Get.off(ThirdPage())"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.offAll(ThirdPage());
+              },
+              child: Text("Get.offAll(ThirdPage())"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.toNamed("/params/test", parameters: {"param": "123"});
+              },
+              child: Text("Get.toNamed(\"/path/test\")"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("This is ThirdPage"),
+          if (Get.arguments != null) Text(Get.arguments),
+          ElevatedButton(
+            onPressed: () {
+              Get.back<Map<String, dynamic>>(result: {"key": "Good"});
+            },
+            child: Text("Get.back(result)"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ParamsPage extends StatelessWidget {
+  const ParamsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(Get.parameters["pathValue"]!),
+            if (Get.parameters["param"] != null) Text(Get.parameters["param"]!),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyDialog extends StatelessWidget {
+  const MyDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 50,
+        width: 50,
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
+class Controller extends GetxController {
+  final routeReturnValue = {}.obs;
 }
